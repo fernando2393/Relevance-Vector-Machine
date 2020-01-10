Variance calculation fixed. Weird r.vectors received in some cases (frd.2, frd.3) --> surprisingly good results

friedman_2.py
@@ -28,13 +28,13 @@ X_training[:,3] = np.random.uniform(1,11, training_samples)
 # Generation of training targets
 training_targets = np.zeros(training_samples)
 
+# Generating target without noise
+training_targets = pow(pow(X_training[:,0], 2) + pow(X_training[:,1] * 
+X_training[:,2] - 1 / (X_training[:,1] * X_training[:,3]), 2), 1/2)
+    
+# Adding noise
 for i in range(training_samples):
-    # Generating target without noise
-    training_targets[i] = pow(pow(X_training[i,0], 2) + pow(X_training[i,1] * 
-    X_training[i,2] - 1 / (X_training[i,1] * X_training[i,3]), 2), 1/2)
-    # Adding noise
-
-training_targets += np.random.normal(0, training_targets.std()/3)
+    training_targets[i] += np.random.normal(0, training_targets.std()/3)
 
 # Reshape to create scalar
 training_targets = np.reshape(training_targets, (len(training_targets),1))
@@ -64,6 +64,7 @@ y = y.mean(axis=1)
 pred_mean = np.array(pred_array).mean(axis=0)
 
 print('RMSE:', sqrt(mean_squared_error(y, pred_mean)))
-plt.scatter(range(test_samples), y)
-plt.scatter(range(test_samples), pred_mean, c='orange')
+plt.scatter(range(test_samples), y, label='Real')
+plt.scatter(range(test_samples), pred_mean, c='orange', label='Predited')
+plt.legend()
 plt.show()