274c758b2c57d4272a4217b6a419d5feaf4019d7
JohnMagnusson <johnvhmagnusson@gmail.com>
2020-01-10 12:45:52 +0100

    Madinf loading of data sets easier

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
 import rvm_classification
-import numpy as np
 
-# Small data set used for development
-# test_data = np.loadtxt("datasets/banana_test_data_dev.asc")
-# test_taraget = np.loadtxt("datasets/banana_test_labels_dev.asc")
+# How to load non predefined data
+# test_data = np.loadtxt("datasets/ripley_test_data.asc")
+# test_target = np.loadtxt("datasets/ripley_test_labels.asc")
 #
-# train_data = np.loadtxt("datasets/banana_train_data_dev.asc")
-# train_taraget = np.loadtxt("datasets/banana_train_labels_dev.asc")
-
-# Banana 1, whole data set
-# test_data = np.loadtxt("datasets/banana_test_data_1.asc")
-# test_taraget = np.loadtxt("datasets/banana_test_labels_1.asc")
-#
-# train_data = np.loadtxt("datasets/banana_train_data_1.asc")
-# train_taraget = np.loadtxt("datasets/banana_train_labels_1.asc")
-
-# Same as the people
-test_data = np.loadtxt("datasets/ripley_test_data.asc")
-test_target = np.loadtxt("datasets/ripley_test_labels.asc")
-
-train_data = np.loadtxt("datasets/ripley_train_data.asc")
-train_target = np.loadtxt("datasets/ripley_train_labels.asc")
+# train_data = np.loadtxt("datasets/ripley_train_data.asc")
+# train_target = np.loadtxt("datasets/ripley_train_labels.asc")
 
 classifier = rvm_classification.RVM_Classifier()
-classifier.train(train_data, train_target)
+# classifier.set_training_data(train_data, train_target)
+
+# Takes the dataset and optional index and set training, test values
+classifier.set_predefined_training_data("banana")
+classifier.train()
 # classifier.predict(test_data)
-classifier.plot(test_data, test_target)
+classifier.plot()
--- rvm_classification.py
+++ rvm_classification.py
@@ -11,8 +11,31 @@ import Kernel
 class RVM_Classifier():
 
     def __init__(self):
+        self.threshold_alpha = 1e5
+        self.bias_used = True
+        self.removed_bias = False
+
         self.trained_weights = None
-        self.relevance_vec = None
+        self.relevance_vector = None
+
+        self.training_data = None
+        self.training_labels = None
+        self.test_data = None
+        self.test_labels = None
+
+    def set_training_data(self, training_data, training_labels):
+        self.training_data = training_data
+        self.training_labels = training_labels
+
+    def set_predefined_training_data(self, data_set, data_set_index=1):
+        self.training_data = np.loadtxt(
+            "datasets/{data_set}/{data_set}_train_data_{index}.asc".format(data_set=data_set, index=data_set_index))
+        self.training_labels = np.loadtxt(
+            "datasets/{data_set}/{data_set}_train_labels_{index}.asc".format(data_set=data_set, index=data_set_index))
+        self.test_data = np.loadtxt(
+            "datasets/{data_set}/{data_set}_test_data_{index}.asc".format(data_set=data_set, index=data_set_index))
+        self.test_labels = np.loadtxt(
+            "datasets/{data_set}/{data_set}_test_labels_{index}.asc".format(data_set=data_set, index=data_set_index))
 
     # Formula 27
     def update_weight_function(self, sigma, phi, beta, target):
@@ -32,21 +55,20 @@ class RVM_Classifier():
             new_alphas[i] = gamma[i] / (weight[i] ** 2 + sys.float_info.epsilon)
         return new_alphas
 
+    # def phi_function(self, x):
+    #     phi = np.ones((x.shape[0], x.shape[0] + 1))
+    #
+    #     for n in range(x.shape[0]):
+    #         for m in range(x.shape[0]):
+    #             phi[n][m + 1] = Kernel.radial_basis_kernel(x[n, :], x[m, :])
+    #     return phi
+
     def phi_function(self, x):
-        phi = np.ones((x.shape[0], x.shape[0] + 1))
+        phi = np.ones((x.shape[0], self.relevance_vector.shape[0] + 1))
 
         for n in range(x.shape[0]):
-            for m in range(x.shape[0]):
-                phi[n][m + 1] = Kernel.radial_basis_kernel(x[n, :], x[m, :])
-        return phi
-
-
-    def phi_function_2(self, x):
-        phi = np.ones((x.shape[0], self.relevance_vec.shape[0] + 1))
-
-        for n in range(x.shape[0]):
-            for m in range(self.relevance_vec.shape[0]):
-                phi[n][m + 1] = Kernel.radial_basis_kernel(x[n, :], self.relevance_vec[m, :])
+            for m in range(self.relevance_vector.shape[0]):
+                phi[n][m + 1] = Kernel.radial_basis_kernel(x[n, :], self.relevance_vector[m, :])
         return phi
 
     # Formula 2
@@ -63,7 +85,7 @@ class RVM_Classifier():
         return y
 
     # From under formula 25
-    def beta_matrix_function(self, y, N, target):
+    def beta_matrix_function(self, y, N):
         beta_matrix = np.zeros((N, N))
         for n in range(N):
             beta_matrix[n][n] = expit(y[n]) * (1 - expit(y[n]))
@@ -94,26 +116,34 @@ class RVM_Classifier():
 
         return log_posterior
 
-    def prune(self):
-        mask = self.alphas < self.threshold_alpha
+    # Todo This function is alot of copy paste ceck it before doing more with it
+    def prune(self, alphas, weights, phi, gamma, sigma):
+        keep_alpha = alphas < self.threshold_alpha
 
-        self.alphas = self.alphas[mask]
-        self.old_alphas = self.old_alphas[mask]
-        self.phi = self.phi[:, mask]
-        self.mu_posterior = self.mu_posterior[mask]
+        if not np.any(keep_alpha):
+            keep_alpha[0] = True
+            if self.bias_used:
+                keep_alpha[-1] = True
 
-        if not self.removed_bias:
-            self.relevance_vec = self.relevance_vec[mask[1:]]
+        if self.bias_used:
+            if not keep_alpha[-1]:
+                self.bias_used = False
+            self.relevance_vector = self.relevance_vector[keep_alpha[:-1]]
         else:
-            self.relevance_vec = self.relevance_vec[mask]
+            self.relevance_vector = self.relevance_vector[keep_alpha]
 
-        if not mask[0] and not self.removed_bias:
-            self.removed_bias = True
-            if self.verbose:
-                print("Bias removed")
+        # if not keep_alpha[0] and not self.removed_bias:
+        #     self.removed_bias = True
+
+
+        return alphas[keep_alpha], weights[keep_alpha], phi[:, keep_alpha], gamma[keep_alpha], sigma[keep_alpha]
 
     # def plot(self, data, data_index_target_list):
-    def plot(self, data, target):
+    def plot(self, data=None, target=None):
+        if data == None and target == None:
+            data = self.test_data
+            target = self.test_labels
+
         # Format data so we can plot it
         print("Will start plotting")
         target_values = np.unique(target)
@@ -144,6 +174,7 @@ class RVM_Classifier():
         for i, c in enumerate(data_index_target_list):
             plt.plot(data[c[1], 0], data[c[1], 1], colors[i], label="Target: " + str(c[0]))
         plt.contour(xx, yy, Z, cmap=plt.cm.Paired)
+        # plt.scatter(self.relevance_vector[:, 0], self.relevance_vector[:, 1], c='black', marker='+', s=500)
         plt.xlabel("Cool label, what should we put here")
         plt.ylabel("Heelloooo lets goo")
         plt.legend()
@@ -153,36 +184,37 @@ class RVM_Classifier():
         """
             Runs the classification
         """
-        phi = self.phi_function_2(data)
+        phi = self.phi_function(data)
         y = self.y_function(self.trained_weights, data, phi)
         # y = np.array([expit(y) for y in estimations])
         pred = np.where(y > 0.5, 1, -1)
         return pred
 
-    def train(self, data, target):
+    def train(self):
         """
          Train the classifier
         """
-        self.relevance_vec = data
+        self.relevance_vector = self.training_data
         max_training_iterations = 100
         threshold = 0.0000000000001
 
+        phi = self.phi_function(self.training_data)
+        # print("phi")
+        # print(phi)
+
         old_log_posterior = float("-inf")
-        weights = np.array([1 / (data.shape[0] + 1)] * (data.shape[0] + 1))  # Initialize uniformly
-        alpha = np.array([1 / (data.shape[0] + 1)] * (data.shape[0] + 1))
+        weights = np.array(
+            [1 / (self.training_data.shape[0] + 1)] * (self.training_data.shape[0] + 1))  # Initialize uniformly
+        alpha = np.array([1 / (self.training_data.shape[0] + 1)] * (self.training_data.shape[0] + 1))
         for i in range(max_training_iterations):
             print("number of iterations:")
             print(i)
 
-            phi = self.phi_function(data)
-            # print("phi")
-            # print(phi)
-
-            y = self.y_function(weights, data, phi)
+            y = self.y_function(weights, self.training_data, phi)
             # print("y")
             # print(y)
 
-            beta = self.beta_matrix_function(y, data.shape[0], target)
+            beta = self.beta_matrix_function(y, self.training_data.shape[0])
             # print("beta")
             # print(beta)
 
@@ -192,19 +224,22 @@ class RVM_Classifier():
 
             gamma = self.gamma_function(alpha, sigma)
 
-            weights = self.update_weight_function(sigma, phi, beta, target)
+            weights = self.update_weight_function(sigma, phi, beta, self.training_labels)
 
             # print("weight")
             # print(weights)
 
             alpha = self.recalculate_alphas_function(alpha, gamma, weights)
+
+            alpha, weights, phi, gamma, sigma = self.prune(alpha, weights, phi, gamma, sigma)
+
             # print("alpha")
             # print(alpha)
 
             # print("y")
             # print(y)
             # print(min(y))
-            log_posterior = self.log_posterior_function(data, weights, target, phi, alpha)
+            log_posterior = self.log_posterior_function(self.training_data, weights, self.training_labels, phi, alpha)
 
             difference = log_posterior - old_log_posterior
             if difference <= threshold:
@@ -213,9 +248,8 @@ class RVM_Classifier():
                 break
             old_log_posterior = log_posterior
 
-            # Format data so we can plot it
-            target_values = np.unique(target)
-            data_index_target_list = [0] * len(target_values)
-            for i, c in enumerate(target_values):  # Saving the data index with its corresponding target
-                data_index_target_list[i] = (c, np.argwhere(target == c))
-            # self.plot(data, data_index_target_list)
+        # Format data so we can plot it
+        target_values = np.unique(self.training_labels)
+        data_index_target_list = [0] * len(target_values)
+        for i, c in enumerate(target_values):  # Saving the data index with its corresponding target
+            data_index_target_list[i] = (c, np.argwhere(self.training_labels == c))
