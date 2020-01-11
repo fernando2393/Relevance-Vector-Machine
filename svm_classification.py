5178f9f18897e402ead65172222ac98a48db874b
Lucasqgongora <lucas.q.gongora@gmail.com>
2020-01-11 18:33:53 +0100

    SVM done

---
+++ svm_classification.py
@@ -0,0 +1,75 @@
+from sklearn.svm import SVC
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix
+
+def get_prediction_error_rate(predicted_targets=[], true_targets=[]):
+    nr_correct = 0
+    for i in range(len(predicted_targets)):
+        if predicted_targets[i] == true_targets[i]:
+            nr_correct += 1
+    return 1 - nr_correct / len(predicted_targets)
+
+data_set = "ripley"
+data_set_index = 1
+training_data = np.loadtxt(
+    "datasets/{data_set}/{data_set}_train_data_{index}.asc".format(data_set=data_set, index=data_set_index))
+training_labels = np.loadtxt(
+    "datasets/{data_set}/{data_set}_train_labels_{index}.asc".format(data_set=data_set, index=data_set_index))
+training_labels[training_labels == -1] = 0  # Sanitize labels, some use -1 and some use 0
+
+test_data = np.loadtxt(
+    "datasets/{data_set}/{data_set}_test_data_{index}.asc".format(data_set=data_set, index=data_set_index))
+test_labels = np.loadtxt(
+    "datasets/{data_set}/{data_set}_test_labels_{index}.asc".format(data_set=data_set, index=data_set_index))
+test_labels[test_labels == -1] = 0  # Sanitize labels, some use -1 and some use 0
+
+#def svm_classification()
+svclassifier = SVC(kernel="rbf", gamma=2)
+svclassifier.fit(training_data, training_labels)
+y_pred = svclassifier.predict(test_data)
+
+#### NUMBER OF SUPPORT VECTORS
+print(sum(svclassifier.n_support_))
+#### ERROR
+print(get_prediction_error_rate(y_pred, test_labels))
+
+#### PLOTTING FOR RIPLEYS
+
+# Format data so we can plot it
+print("Will start plotting")
+data = training_data
+target = training_labels
+
+target_values = np.unique(target)
+data_index_target_list = [0] * len(target_values)
+for i, c in enumerate(target_values):  # Saving the data index with its corresponding target
+    data_index_target_list[i] = (c, np.argwhere(target == c))
+
+h = 0.01  # step size in the mesh
+# create a mesh to plot in
+x_min, x_max = data[:, 0].min(), data[:, 0].max()  # Gets the max and min value of x in the data
+y_min, y_max = data[:, 1].min(), data[:, 1].max()  # Gets the max and min value of y in the data
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                        np.arange(y_min, y_max, h))  # Creates a mesh from max and min with step size h
+
+# Plot the decision boundary. For that, we will assign a color to each
+# point in the mesh [x_min, m_max]x[y_min, y_max].
+print("Calculating the prediction, this might take a while...")
+data_mesh = np.c_[xx.ravel(), yy.ravel()]
+Z = svclassifier.predict(data_mesh)
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+
+colors = ["bx", "go", "ro", "co", "mo", "yo", "bo", "wo"]
+plt.figure(figsize=(12, 6))
+plt.title('Banana dataset')
+plt.contour(xx, yy, Z, cmap=plt.cm.Paired)
+for i, c in enumerate(data_index_target_list):
+    plt.plot(data[c[1], 0], data[c[1], 1], colors[i], label="Target: " + str(c[0]))
+#plt.scatter(self.relevance_vector[:, 0], self.relevance_vector[:, 1], c='black', marker='+', s=500)
+plt.xlabel("Cool label, what should we put here")
+plt.ylabel("Heelloooo lets goo")
+plt.legend()
+plt.show()
