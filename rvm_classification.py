47131d3ae435eaea76892a71de62807a8db265be
Lucasqgongora <lucas.q.gongora@gmail.com>
2020-01-12 21:52:13 +0100

    fixing ripley input

--- rvm_classification.py
+++ rvm_classification.py
@@ -62,7 +62,7 @@ class RVM_Classifier:
             self.training_data = np.loadtxt("datasets/ripley/ripley_train_data.asc")
             self.training_labels = np.loadtxt("datasets/ripley/ripley_train_labels.asc")
             self.test_data = np.loadtxt("datasets/ripley/ripley_test_data.asc")
-            self.test_labels = np.loadtxt("datasets/ripley/ripley_test_data.asc")
+            self.test_labels = np.loadtxt("datasets/ripley/ripley_test_labels.asc")
         else:
             self.training_data = np.loadtxt(
                 "datasets/{data_set}/{data_set}_train_data_{index}.asc".format(data_set=data_set, index=data_set_index))
@@ -159,7 +159,7 @@ class RVM_Classifier:
     def hessian(self, weights, alphas, phi, target):
         y = self.y_function(weights, phi)
         beta = self.beta_matrix_function(y)
-        return np.diag(alphas) + np.linalg.multi_dot(phi.T, beta, phi)
+        return np.diag(alphas) + np.linalg.multi_dot([phi.T, beta, phi])
 
     def update_weights(self):
         result = minimize(
@@ -285,7 +285,7 @@ class RVM_Classifier:
         for i, c in enumerate(target_values):  # Saving the data index with its corresponding target
             data_index_target_list[i] = (c, np.argwhere(target == c))
 
-        h = 0.1  # step size in the mesh
+        h = 0.01  # step size in the mesh
         # create a mesh to plot in
         x_min, x_max = data[:, 0].min(), data[:, 0].max()  # Gets the max and min value of x in the data
         y_min, y_max = data[:, 1].min(), data[:, 1].max()  # Gets the max and min value of y in the data
@@ -301,17 +301,21 @@ class RVM_Classifier:
         # Put the result into a color plot
         Z = Z.reshape(xx.shape)
 
-        colors = ["bx", "go", "ro", "co", "mo", "yo", "bo", "wo"]
+        colors = ["rx", "bx"]
         plt.figure(figsize=(12, 6))
-        plt.title('Banana dataset')
-        plt.contour(xx, yy, Z, cmap=plt.cm.Paired)
+        plt.title('')
+        plt.contour(xx, yy, Z, cmap= plt.get_cmap('Greys'))
         for i, c in enumerate(data_index_target_list):
             plt.plot(data[c[1], 0], data[c[1], 1], colors[i], label="Target: " + str(c[0]))
-        plt.scatter(self.relevance_vector[:, 0], self.relevance_vector[:, 1], c='black', marker='+', s=500)
-        plt.xlabel("Cool label, what should we put here")
-        plt.ylabel("Heelloooo lets goo")
+        plt.scatter(self.relevance_vector[:, 0], self.relevance_vector[:, 1], c='black', marker = 'o', s=80, alpha=0.5)
+        plt.xlabel("")
+        plt.ylabel("")
         plt.legend()
+        plt.grid('on')
+        plt.gca().spines["top"].set_visible(False)
+        plt.gca().spines["right"].set_visible(False)
         plt.show()
+
 
     def get_prediction_error_rate(self, predicted_targets=[], true_targets=[], use_predefined_training=False):
         if predicted_targets == [] and true_targets == []:
