a3f67c15dd15943a5cd9c12fc23d281c8439e3e5
fernando2393 <garciasanzfernando2393@gmail.com>
2020-01-12 20:37:45 +0100

    Absolute value moved to kernel

--- svm_methods.py
+++ svm_methods.py
@@ -1,11 +1,13 @@
 import numpy as np
 
 def linear_spline(x_m, x_n):
-    compute_kernel = np.zeros((x_m.shape[0], x_n.shape[0]))
-    for i in range(x_m.shape[0]):
-        for j in range(x_n.shape[0]):
-            xmin = np.minimum(x_m[i], x_n[j])
-            compute_kernel[i,j] = (1 + x_m[i] * x_n[j] + x_m[i] * x_n[j] * xmin - 
-            ((x_m[i] + x_n[j]) / 2) * pow(xmin, 2) + pow(xmin, 3) / 3).prod()
+    x_m_aux = abs(x_m)
+    x_n_aux = abs(x_n)
+    compute_kernel = np.zeros((x_m_aux.shape[0], x_n_aux.shape[0]))
+    for i in range(x_m_aux.shape[0]):
+        for j in range(x_n_aux.shape[0]):
+            xmin = np.minimum(x_m_aux[i], x_n_aux[j])
+            compute_kernel[i,j] = (1 + x_m_aux[i] * x_n_aux[j] + x_m_aux[i] * x_n_aux[j] * xmin - 
+            ((x_m_aux[i] + x_n_aux[j]) / 2) * pow(xmin, 2) + pow(xmin, 3) / 3).prod()
 
     return compute_kernel
