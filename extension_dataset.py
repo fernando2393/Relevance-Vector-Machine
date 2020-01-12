5606a97a56dbfdffefe872535d20d86f5f6882b3
Gustavo Beck <teodoro.beck@gmail.com>
2020-01-12 06:57:58 +0100

    Extension data set done

--- extension_dataset.py
+++ extension_dataset.py
@@ -11,9 +11,13 @@ from sklearn.svm import SVR
 import itertools
 
 # Initialize variable
-N = 100
+N = 10
 dimensions = 2
-N_test = 1000
+N_test = 100
+variance = 0.01
+
+# Choose kernel between linear_spline, gaussian or exponential
+kernel = "linear_spline"
 
 X_1 = np.linspace(-10,10,N) # Training
 X_2 = np.linspace(-10,10,N) # Training
@@ -21,49 +25,38 @@ X = np.zeros(((N)**2, 2))
 for i, x in enumerate(itertools.product(X_1, X_2)):
     X[i, :] = x
 
+y_train = np.zeros(X.shape[0])
+
+for i in range(X.shape[0]):
+    y_train[i] = math.sin(X[i, 0]) / X[i, 0] + 0.1 * X[i, 1] + np.random.normal(0,0.1)
+
 X_1_test = np.linspace(-10,10,N_test) # Testing
 X_2_test = np.linspace(-10,10,N_test) # Testing
 X_test = np.zeros(((N_test)**2, 2))
 for i, x in enumerate(itertools.product(X_1_test, X_2_test)):
     X_test[i, :] = x
 
-variance = 0.01
-tests = 100
-pred_array = list() # Average regression
-
-# Choose kernel between linear_spline or exponential
-kernel = "exponential"
-
-targets = np.zeros(X.shape[0])
-
-for i in tqdm(range(X.shape[0])):
-    targets[i] = math.sin(X[i, 0]) / X[i, 0] + 0.1 * X[i, 1] + np.random.normal(0, 0.1)
-
-alpha, variance_mp, mu_mp, sigma_mp = rvm_r.fit(X, variance, targets, kernel, X.shape[0])
+alpha, variance_mp, mu_mp, sigma_mp = rvm_r.fit(X, variance, y_train, kernel, X.shape[0])
 relevant_vectors = alpha[1].astype(int)
 
-targets_test = np.zeros(X_test.shape[0])
 y = np.zeros(X_test.shape[0])
+for i in range(X_test.shape[0]):
+    y[i] = math.sin(X_test[i, 0]) / X_test[i, 0] + 0.1 * X_test[i, 1]
 
-for it in tqdm(range(tests)):
-    for i in range(X_test.shape[0]):
-        targets_test[i] =  math.sin(X_test[i, 0]) / X_test[i, 0] + 0.1 * X_test[i, 1] + np.random.normal(0, 0.1)
-        y[i] = math.sin(X_test[i, 0]) / X_test[i, 0] + 0.1 * X_test[i, 1]
-    pred_array.append(rvm_r.predict(X, X_test, relevant_vectors, variance_mp, mu_mp, sigma_mp, kernel, dimensions))
-pred_mean = np.array(pred_array).mean(axis=0)
-print('RMSE:', sqrt(mean_squared_error(y, pred_mean)))
-print('Maximum error between predicted samples and true: ', max(abs(y-pred_mean))**2)
+y_pred = rvm_r.predict(X, X_test, relevant_vectors, variance_mp, mu_mp, sigma_mp, kernel, dimensions)
+
+print('RMSE:', sqrt(mean_squared_error(y, y_pred)))
+print('Maximum error between predicted samples and true: ', max(abs(y-y_pred))**2)
 print('Number of relevant vectors:', len(relevant_vectors)-1)
 
 sns.set(style="darkgrid")
 fig = plt.figure()
 fig.set_size_inches(15, 11)
 ax = fig.gca(projection='3d')
-ax.invert_xaxis()
-ax.plot_trisurf(X_test[:,0], X_test[:,1], y, cmap=plt.cm.viridis, linewidth=0.2)
-ax.scatter(X[:,0], X[:,1], targets, cmap=plt.cm.viridis, linewidth=0.2, label='Training points')
+ax.plot_trisurf(X_test[:,0], X_test[:,1], y_pred, cmap=plt.cm.plasma, linewidth=0.2)
 ax.view_init(30, -50)
+ax.scatter(X[:,0], X[:,1], y_train, c = "black", label='Training points')
 ax.set_xlabel('x1'); ax.set_ylabel('x2'); ax.set_zlabel('y')
-plt.title("Extension Data Set")
+plt.title("Extension data set prediction")
 plt.legend()
 plt.show()
