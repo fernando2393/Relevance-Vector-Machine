9d99251cb5b6c4ebf98e1550fc30b0feab6480ab
Lucasqgongora <lucas.q.gongora@gmail.com>
2020-01-12 13:46:42 +0100

    svm done

--- svm_classification.py
+++ svm_classification.py
@@ -1,7 +1,24 @@
-from sklearn.svm import SVC
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import cross_val_score
+from sklearn.svm import SVC
+
+
+def get_nr_random_samples(data, target, nr_samples):
+    total_nr_samples = data.shape[0]
+    rnd_indexes = random.sample(range(total_nr_samples), nr_samples)
+
+    random_data = []
+    random_target = []
+    for index in rnd_indexes:
+        random_data.append(data[index])
+        random_target.append(target[index])
+
+    random_data = np.array(random_data)
+    random_target = np.array(random_target)
+    return random_data, random_target
 
 def get_prediction_error_rate(predicted_targets=[], true_targets=[]):
     nr_correct = 0
@@ -12,11 +29,11 @@ def get_prediction_error_rate(predicted_targets=[], true_targets=[]):
 
 data_set = "ripley"
 data_set_index = 1
-training_data = np.loadtxt(
+train_data = np.loadtxt(
     "datasets/{data_set}/{data_set}_train_data_{index}.asc".format(data_set=data_set, index=data_set_index))
-training_labels = np.loadtxt(
+train_labels = np.loadtxt(
     "datasets/{data_set}/{data_set}_train_labels_{index}.asc".format(data_set=data_set, index=data_set_index))
-training_labels[training_labels == -1] = 0  # Sanitize labels, some use -1 and some use 0
+train_labels[train_labels == -1] = 0  # Sanitize labels, some use -1 and some use 0
 
 test_data = np.loadtxt(
     "datasets/{data_set}/{data_set}_test_data_{index}.asc".format(data_set=data_set, index=data_set_index))
@@ -24,22 +41,82 @@ test_labels = np.loadtxt(
     "datasets/{data_set}/{data_set}_test_labels_{index}.asc".format(data_set=data_set, index=data_set_index))
 test_labels[test_labels == -1] = 0  # Sanitize labels, some use -1 and some use 0
 
+
+seed_ = -1
+predicted_error_aux = 101
+best_seed = 0
+
+for seed in range(1000):
+    print(seed)
+    seed = seed_+1
+    np.random.seed(seed)
+
+    train_data, train_labels = get_nr_random_samples(train_data, train_labels, 100)
+
+    #def svm_classification()
+    c_range = np.array([0.001, 0.01, 0.1, 1, 10, 100, 1000])
+    k_fold = 5
+
+    score = np.zeros(len(c_range))
+    for c in range(len(c_range)):
+        svclassifier = SVC(kernel="rbf", gamma=4, C=c_range[c])
+        score[c] = np.mean(cross_val_score(svclassifier, train_data, train_labels, cv=k_fold))
+        
+
+    c = c_range[np.argmax(score)]
+    #print("this is  the best c {c}".format(c = c))
+    svclassifier = SVC(kernel="rbf", gamma=4, C=c)
+
+    svclassifier.fit(train_data, train_labels)
+    y_pred = svclassifier.predict(test_data)
+    predicted_error = get_prediction_error_rate(y_pred, test_labels)
+    
+if predicted_error < predicted_error_aux:
+        best_seed = seed
+        predicted_error_aux = predicted_error
+
+
+np.random.seed(best_seed)
+print("the best seed is: {seed}".format(seed=best_seed))
+train_data, train_labels = get_nr_random_samples(train_data, train_labels, 100)
+'''
+this is  the best c 1.0
+total number of support vectors: 41
+the predict error is 0.10999999999999999
+'''
 #def svm_classification()
-svclassifier = SVC(kernel="rbf", gamma=2)
-svclassifier.fit(training_data, training_labels)
+c_range = np.array([0.001, 0.01, 0.1, 1, 10, 100, 1000])
+k_fold = 5
+
+score = np.zeros(len(c_range))
+for c in range(len(c_range)):
+    svclassifier = SVC(kernel="rbf", gamma=4, C=c_range[c])
+    score[c] = np.mean(cross_val_score(svclassifier, train_data, train_labels, cv=k_fold))
+    
+
+c = c_range[np.argmax(score)]
+print("this is  the best c {c}".format(c = c))
+svclassifier = SVC(kernel="rbf", gamma=4, C=c)
+
+svclassifier.fit(train_data, train_labels)
 y_pred = svclassifier.predict(test_data)
+predicted_error = get_prediction_error_rate(y_pred, test_labels)    
+    
+
+relevance_vector = svclassifier.support_vectors_
 
 #### NUMBER OF SUPPORT VECTORS
-print(sum(svclassifier.n_support_))
+total_support_vector = sum(svclassifier.n_support_)
+print("total number of support vectors: {tsv}".format(tsv = total_support_vector))
 #### ERROR
-print(get_prediction_error_rate(y_pred, test_labels))
-
+predict_error = get_prediction_error_rate(y_pred, test_labels)
+print("the predict error is {error}".format(error = predict_error))
 #### PLOTTING FOR RIPLEYS
 
 # Format data so we can plot it
 print("Will start plotting")
-data = training_data
-target = training_labels
+data = train_data
+target = train_labels
 
 target_values = np.unique(target)
 data_index_target_list = [0] * len(target_values)
@@ -58,18 +135,20 @@ xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
 print("Calculating the prediction, this might take a while...")
 data_mesh = np.c_[xx.ravel(), yy.ravel()]
 Z = svclassifier.predict(data_mesh)
-
 # Put the result into a color plot
 Z = Z.reshape(xx.shape)
 
-colors = ["bx", "go", "ro", "co", "mo", "yo", "bo", "wo"]
+colors = ["rx", "bx"]
 plt.figure(figsize=(12, 6))
-plt.title('Banana dataset')
-plt.contour(xx, yy, Z, cmap=plt.cm.Paired)
+plt.title('')
+plt.contour(xx, yy, Z, cmap= plt.get_cmap('Greys'))
 for i, c in enumerate(data_index_target_list):
     plt.plot(data[c[1], 0], data[c[1], 1], colors[i], label="Target: " + str(c[0]))
-#plt.scatter(self.relevance_vector[:, 0], self.relevance_vector[:, 1], c='black', marker='+', s=500)
-plt.xlabel("Cool label, what should we put here")
-plt.ylabel("Heelloooo lets goo")
+plt.scatter(relevance_vector[:, 0], relevance_vector[:, 1], c='black', marker = 'o', s=80, alpha=0.5)
+plt.xlabel("")
+plt.ylabel("")
 plt.legend()
+plt.grid('on')
+plt.gca().spines["top"].set_visible(False)
+plt.gca().spines["right"].set_visible(False)
 plt.show()
